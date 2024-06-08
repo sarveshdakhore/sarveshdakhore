e10c2f399aae2102ddfebc25b036035ea3a518c7
sarveshdakhore <sarveshd444@gmail.com>
2024-06-09 02:45:15 +0530

    ;

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,6 @@ export default function RootLayout({
     <>
       <Head>
         <meta name="google-adsense-account" content="ca-pub-7726311284531292" />
-        <title>{metadata.title || 'Default Title'}</title>
         <meta name="description" content={metadata.description || 'Default Description'} />
       </Head>
       <div className={inter.className}>{children}</div>
