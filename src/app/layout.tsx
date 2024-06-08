36c2411970e75199357afc61947a00b1c1f31c28
sarveshdakhore <sarveshd444@gmail.com>
2024-06-09 02:28:17 +0530

    portfolio change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,6 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <meta name="google-adsense-account" content="ca-pub-7726311284531292" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
