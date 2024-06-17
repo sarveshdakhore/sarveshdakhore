5cd8ee292a79203d8c924e0c02e1b4964bdec4c3
sarveshdakhore <sarveshd444@gmail.com>
2024-06-17 10:28:15 +0530

    cxc

--- src/app/privacy_policy/page.tsx
+++ src/app/privacy_policy/page.tsx
@@ -1,5 +1,12 @@
 export default function Redirect() {
   return (
-  <main>the side sarveshdakhore.in directly does not collect any kind of information from user, the ffcs.sarveshdakhore is fully frontend application and third party code is being included in the frontend code, we dont take any responsibity for the data collected by third pary. </main>
+    <main>
+      <p>
+        The site sarveshdakhore.in directly does not collect any kind of information from user, 
+        the ffcs.sarveshdakhore is fully frontend application and third party code is being 
+        included in the frontend code, we don't take any responsibility for the data collected 
+        by third party.
+      </p>
+    </main>
   )
 }
